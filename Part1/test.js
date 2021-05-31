4d785fdf764c9e9109a00e62c45392eaa534e567
Archana Joshi <59567780+ArchanaNjoshi@users.noreply.github.com>
2021-05-31 16:08:48 +0530

    Add files via upload

---
+++ Part1/test.js
@@ -0,0 +1,10 @@
+/* const wholeTable=document.querySelector('.div-para');
+const table=document.querySelector('.table');
+const row1=document.querySelector('.row1');
+const row2=document.querySelector('.row2');
+const data1=document.querySelector('.data1');
+const data2=document.querySelector('.data2');
+
+data2.innerHTML='changed data2ddddd';
+console.log(data2.textContent); 
+console.log(table.textContent);  */
